93a9e0a130653fc9737d86201997ff91d1c231f1
Sil van Diepen <me@silvandiepen.com>
2019-09-02 13:42:35 +0200

    add more options

--- src/slider.vue
+++ src/slider.vue
@@ -1,8 +1,7 @@
 <template>
 	<div class="slider" @click="nextSlide(true)">
-		<div class="slider__count">
-			<span>{{ slider.current }} / {{ slideList.length }}</span>
-		</div>
+
+		<!-- The slides -->
 		<div v-if="slideList">
 			<div
 				v-for="(image, idx) in slideList"
@@ -18,6 +17,30 @@
 					/>
 				</div>
 			</div>
+		</div>
+		
+		<!-- Counter  -->
+		<div class="slider__count" v-if="slider.counter">
+			<span>{{ slider.current }} / {{ slideList.length }}</span>
+		</div>
+		
+		<!-- Navigation Arrows -->
+		<div class="slider__arrows" v-if="slider.arrows">
+			<button class="slider__arrow-prev" @click="prevSlide(true)"></button>
+			<button class="slider__arrow-next" @click="nextSlide(true)"></button>
+		</div>
+		
+		<!-- Navigation bars -->
+		<div class="slider__nav" v-if="slider.navigation">
+			<ul class="slider__nav-list">
+				<li
+					class="slider__nav-item"
+					v-for="(slide, idx) in slideList"
+					:key="idx"
+				>
+					<button class="slider__nav-button">{{ idx }}</button>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
@@ -37,16 +60,21 @@ export default {
 			default: () => {}
 		}
 	},
-	data: () => ({
-		slider: {
-			active: true,
-			autoplay: true,
-			autoplaySpeed: 3000,
-			startAt: 0,
-			current: this.$props.startAt || 0,
-			autoplay: this.$props.autoplay || true
-		}
-	}),
+	data() {
+		return {
+			slider: {
+				active: true,
+				autoplay: true,
+				autoplaySpeed: 3000,
+				navigation: false,
+				arrows: false,
+				counter: false,
+				startAt: 0,
+				current: this.$props.startAt || 0,
+				autoplay: this.$props.autoplay || true
+			}
+		};
+	},
 	computed: {
 		slideList() {
 			if (this.$props.slides) return this.$props.slides;
@@ -64,8 +92,21 @@ export default {
 	},
 	methods: {
 		nextSlide(clicked = false) {
-			if (this.slider.current + 1 > this.slideList.length - 1) this.slider.current = 0;
+			if (this.slider.current + 1 > this.slideList.length - 1)
+				this.slider.current = 0;
 			else this.slider.current++;
+
+			if (clicked) {
+				this.slider.autoplay = false;
+				setTimeout(() => {
+					this.slider.autoplay = true;
+				}, this.slider.autoplaySpeed);
+			}
+		},
+		prevSlide(clicked = false) {
+			if (this.slider.current - 1 < 0)
+				this.slider.current = this.slideList.length;
+			else this.slider.current = this.slider.current - 1;
 
 			if (clicked) {
 				this.slider.autoplay = false;
